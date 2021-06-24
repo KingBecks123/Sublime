8c2cf3a4256ce345d7c63125af5a86da6d23f01b
KingBecks123 <46463309+KingBecks123@users.noreply.github.com>
2021-06-24 17:30:52 +0100

    Add files via upload

--- myfunctions.js
+++ myfunctions.js
@@ -4,7 +4,7 @@ function loadStuff(savegame) {
     if (savegame !== null) {
         Object.assign(gameData, savegame);
         backwardsCompatibility(savegame.versionNumber)
-        gameData.versionNumber = 99
+        gameData.versionNumber = 100
         updateValues()
         updateAfterLoad()
     } else {
