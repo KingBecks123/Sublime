ee0bea49abddb1f3f618d702c7cab93f573a1b63
Becky's Sublime <beckhamjanzen@gmail.com>
2025-03-18 22:19:32 +0000

    V1.1.6

--- js/base/data.js
+++ js/base/data.js
@@ -1,11 +1,7 @@
-const GAME_VERSION = "1.1.5";
+const GAME_VERSION = "1.1.6";
 const VERSION_NOTES = 
-`16/Mar/2025
-    Fixed issue with lore hooks
-    Added sfx toggle button with a cool color change
-    Fixed issue with wheat harvester and seed drill
-    Updated audio system to handle multiple sounds better
-`;
+`18/Mar/2025
+    An uncountable number of backend changes
 
 mainTabs = [
 	{
